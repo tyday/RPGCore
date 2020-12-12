91f65dd5d7f5ca0bddb818c7f486f275e60e9976
Ty Day <tyrday@gmail.com>
2020-12-12 16:17:20 -0500

    first commit

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    void Update()
+    {
+        transform.position = target.position;
+    }
+}
