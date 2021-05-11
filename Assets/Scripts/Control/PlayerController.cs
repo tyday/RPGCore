0d777af6a7b4242eaecb6b0c45c39033228b2b6d
Ty Day <tyrday@gmail.com>
2021-05-11 15:03:13 -0400

    Adjusted core namespaces

---
+++ Assets/Scripts/Control/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using RPG.Movement;
+namespace RPG.Control
+{
+    public class PlayerController : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetMouseButton(0))
+            {
+                MoveToCursor();
+            }
+        }
+        private void MoveToCursor()
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            bool hasHit = Physics.Raycast(ray, out hit);
+            if (hasHit)
+            {
+                GetComponent<Mover>().MoveTo(hit.point);
+            }
+        }
+    }
+
+}
